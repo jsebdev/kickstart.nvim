c960cb01465b80d744b4641fe893bcb4e828df2a
Sebastian Caicedo <jsebdev@gmail.com>
2025-03-24 16:31:03 -0500

    custom even more nvimtree

--- lua/custom/plugin_config/nvimtree.lua
+++ lua/custom/plugin_config/nvimtree.lua
@@ -1,8 +1,19 @@
 local M = {}
 
 function M.setup()
-  require('nvim-tree').setup {}
-  vim.keymap.set('n', '<A-b>', ':NvimTreeToggle<cr>', { noremap = true, silent = true })
+  require('nvim-tree').setup {
+    view = {
+      width = 30,
+    },
+  }
+  local keymap = vim.keymap.set
+  local opts = { noremap = true, silent = true }
+
+  -- keymap('n', '<C-n>', ':NvimTreeToggle<CR>', opts)
+  keymap('n', '<leader>e', ':NvimTreeToggle<CR>', opts)
+  -- keymap('n', '<leader>n', ':NvimTreeFocus<CR>', opts)
+  keymap('n', '<leader>f', ':NvimTreeFindFile<CR>', opts)
+  -- keymap('n', '<leader>c', ':NvimTreeCollapse<CR>', opts)
 end
 
 return M
