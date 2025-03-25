a5d71e96c2b6590cd4bf55b9ff9d5d7b38ed9f6e
Sebastian <sebastian@macheight.com>
2025-03-25 18:48:56 -0500

    remap info in nvim-tree

--- lua/custom/plugin_config/nvimtree.lua
+++ lua/custom/plugin_config/nvimtree.lua
@@ -1,11 +1,28 @@
 local M = {}
 local map = require('utils.keymap').defaultMap
 
+local function my_on_attach(bufnr)
+  local api = require 'nvim-tree.api'
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+  vim.keymap.del('n', '<C-k>', { buffer = bufnr })
+  vim.keymap.set('n', '<C-i>', api.node.show_info_popup, opts 'Info')
+  print 'nvim tree attach'
+end
+
 function M.setup()
   require('nvim-tree').setup {
     view = {
       width = 30,
     },
+    on_attach = my_on_attach,
   }
 
   map('n', '<leader>ee', ':NvimTreeToggle<CR>', 'Open NvimTree')
