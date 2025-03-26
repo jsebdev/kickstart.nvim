7c753b5bdd4558318bfcf28999addf43ab5b4935
Sebastian <sebastian@macheight.com>
2025-03-26 12:03:02 -0500

    make keymap to reset nvim tree to root directory

--- lua/custom/plugin_config/nvimtree.lua
+++ lua/custom/plugin_config/nvimtree.lua
@@ -16,6 +16,9 @@ local function my_on_attach(bufnr)
   vim.keymap.set('n', '<C-i>', api.node.show_info_popup, opts 'Info')
 end
 
+-- Store the initial root (directory you opened with `nvim .`)
+_G.nvim_tree_initial_root = vim.fn.getcwd()
+
 function M.setup()
   require('nvim-tree').setup {
     view = {
@@ -26,6 +29,15 @@ function M.setup()
 
   map('n', '<leader>ee', ':NvimTreeToggle<CR>', 'Open NvimTree')
   map('n', '<leader>ef', ':NvimTreeFindFile<CR>', 'Find file in NvimTree')
+
+  vim.keymap.set("n", "<leader>er", function()
+    local api = require("nvim-tree.api")
+    local root = vim.fn.getcwd()
+    print("the roor dir is " .. _G.nvim_tree_initial_root)
+    api.tree.change_root(_G.nvim_tree_initial_root)
+    api.tree.reload()
+  end, { desc = "[E]xplore [R]eset NvimTree to project root" })
+
 end
 
 return M
