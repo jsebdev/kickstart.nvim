8264f4b114f6289a14c4e2b1c60ce2ccfd8e8829
Sebastian <sebastian@macheight.com>
2025-03-27 09:41:11 -0500

    use ruff for python formating

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -50,3 +50,6 @@ vim.keymap.set('v', '<C-_>', 'gc', { remap = true, desc = 'comment single line' 
 
 -- smart selection ignoring escaped quotes
 vim.keymap.set({ 'o', 'x' }, 'iq', require('custom.utils.select_inside_unescaped_quotes').select_inside_unescaped_quotes, { desc = 'smart inside quotes' })
+
+-- formating
+vim.keymap.set({'n', 'x'}, '=', function() vim.lsp.buf.format {async = false} end, { desc = 'Autoformat'})
