b7591fae597cdaf9d99aef7e63e42c518674fe82
Sebastian Caicedo <jsebdev@gmail.com>
2025-04-13 20:19:23 -0500

    avoid autocomplete for first match on command line

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -15,4 +15,5 @@ vim.opt.spelllang = { 'en_us' }
 vim.api.nvim_set_hl(0, "Normal", { bg = "#111111", fg = "#bbbbcc" })
 vim.api.nvim_set_hl(0, "Visual", { bg = "#555555", fg = "#000000" })
 
-
+-- autocomplete in command line
+vim.opt.wildmode = "longest:full,full"
