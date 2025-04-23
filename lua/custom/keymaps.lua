3f52010b055b7006da09a4b97b7de2a61d967ae1
Sebastian Caicedo <jsebdev@gmail.com>
2025-04-22 20:07:04 -0500

    allow switch windows while in visual mode

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -161,9 +161,9 @@ vim.keymap.set('i', '<A-h>', [[<C-\><C-N><C-w>h]], opts)
 vim.keymap.set('i', '<A-j>', [[<C-\><C-N><C-w>j]], opts)
 vim.keymap.set('i', '<A-k>', [[<C-\><C-N><C-w>k]], opts)
 vim.keymap.set('i', '<A-l>', [[<C-\><C-N><C-w>l]], opts)
-vim.keymap.set('n', '<A-h>', '<C-w>h', opts)
-vim.keymap.set('n', '<A-j>', '<C-w>j', opts)
-vim.keymap.set('n', '<A-k>', '<C-w>k', opts)
-vim.keymap.set('n', '<A-l>', '<C-w>l', opts)
+vim.keymap.set({'n', 'v'}, '<A-h>', '<C-w>h', opts)
+vim.keymap.set({'n', 'v'}, '<A-j>', '<C-w>j', opts)
+vim.keymap.set({'n', 'v'}, '<A-k>', '<C-w>k', opts)
+vim.keymap.set({'n', 'v'}, '<A-l>', '<C-w>l', opts)
 
 
