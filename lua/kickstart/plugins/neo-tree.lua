3ca9325d46b7b74779219dba6eaabde3c352a05c
Sebastian <sebastian@macheight.com>
2025-04-24 17:29:37 -0500

    remove f mapping from filesystem

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -285,7 +285,6 @@ return {
               ["D"] = "fuzzy_finder_directory",
               ["#"] = "fuzzy_sorter", -- fuzzy sorting using the fzy algorithm
               -- ["D"] = "fuzzy_sorter_directory",
-              ["f"] = "filter_on_submit",
               ["<c-x>"] = "clear_filter",
               ["[g"] = "prev_git_modified",
               ["]g"] = "next_git_modified",
