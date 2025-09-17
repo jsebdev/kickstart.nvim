7d74348cf9968a2a75cfe1b66c5bd4703b6ffd93
Sebastian Caicedo <jsebdev@gmail.com>
2025-09-17 10:02:23 -0500

    format with prettier files in typescript and javascript

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -30,7 +30,7 @@ return {
 
       local sources = {
         diagnostics.checkmake,
-        formatting.prettier.with { filetypes = { 'html', 'json', 'yaml', 'markdown' } },
+        formatting.prettier.with { filetypes = { 'html', 'json', 'yaml', 'markdown', 'javascript', 'typescript', 'typescriptreact', 'javascriptreact' } },
         formatting.stylua,
         formatting.shfmt.with { args = { '-i', '4' } },
         formatting.terraform_fmt,
