ba550dd8aeb8fecf3d5ce768f6b9f116ce3ad3e4
Sebastian Caicedo <jsebdev@gmail.com>
2025-09-25 06:17:31 -0500

    Revert "Tried installing hologram to see images"
    This reverts commit 8c401c475aa28c9561f341a343890802268bd877.

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -204,12 +204,4 @@ return {
       require("lsp-file-operations").setup()
     end,
   },
-  {
-    "edluffy/hologram.nvim",
-    config = function()
-      require('hologram').setup{
-        auto_display = true -- WIP automatic markdown image display, may be prone to breaking
-      }
-    end
-  },
 }
